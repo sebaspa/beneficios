61010a377718106cfe06075101a920036e2994b7
juan_parra_m <juan.parra.m@ab-inbev.com>
2023-02-28 16:36:47 -0500

    Login Validacion de formulario Traducciones de errores

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router";
 import { MainLayout } from "./layouts";
 import { Home, Login } from "./pages";
+import { Test } from "./pages/Test";
 
 export const App = () => {
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
+      <Route path="/test" element={<Test />} />
       <Route path="/" element={<MainLayout />}>
         <Route index element={<Home />} />
       </Route>
---
+++ src/pages/Test.tsx
@@ -0,0 +1,11 @@
+export const Test = () => {
+  var colorbd = "#ff0000";
+  return (
+    <>
+    <div className={`w-40 h-40 bg-[${colorbd}]`}>
+      <p className="text-yellow-600">Test</p>
+    </div>
+    
+    </>
+  )
+}
